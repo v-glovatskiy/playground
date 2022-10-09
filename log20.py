a563e406797dac06e7f4bdd254ea163a92b058d4
Vova <vova.glovatskiy@gmail.com>
2022-10-09 09:11:42 +0300

    log20.py

---
+++ log20.py
@@ -0,0 +1,39 @@
+
+from sys import argv
+import os
+
+if len(argv)<2:
+    print ('Fatal: you forgot to enter an argument.')
+else:
+    print('argument exists')
+script, filename = argv
+
+name = 'COMPLETED after 1'
+if os.access((filename), os.F_OK): 
+    print ('Given file path is exist.')
+
+if os.access((filename), os.R_OK):
+    print ('File is accessible to read')
+ 
+if os.access((filename), os.W_OK): 
+    print ('File is accessible to write')
+ 
+else:
+    print('no such file')
+
+
+txt = open(filename)
+
+print ('Содержимое файла %r' % filename)
+#print (txt.read())
+
+for string in txt:
+
+    if name in string:
+                   
+        print(string)
+
+
+
+    
+
