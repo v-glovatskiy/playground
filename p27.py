c0cd6b092ff76ad9f5a25ede0cd7d14ae6190d18
Vova <vova.glovatskiy@gmail.com>
2023-01-07 10:17:25 +0300

    p27.py

---
+++ p27.py
@@ -0,0 +1,19 @@
+
+import os
+import sys
+try:
+    for entry in os.scandir(sys.argv[1]):
+        if entry.is_dir():
+            typ = 'dir'
+        elif entry.is_file():
+            typ = 'file'
+        elif entry.is_symlink():
+            typ = 'link'
+    else:
+        typ = 'unknown'
+    print('{name} {typ}'.format(
+        name=entry.name,
+        typ=typ,
+    ))
+except:
+    print('No such folder')
