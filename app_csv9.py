5c827ec2734c303081ef453c077cb0425121a787
Vova <vova.glovatskiy@gmail.com>
2023-02-05 16:53:24 +0300

    app_csv9.py

--- app_csv9.py
+++ app_csv9.py
@@ -1,8 +1,40 @@
 
+from sys import argv
+import os
 import csv
+
+if len(argv)<2:
+    print ('Fatal: you forgot to enter an argument.')
+else:
+    print('argument exists')
+script, filename = argv
+
 name = 'ERROR'
-with open('app_2023_jan.csv', newline='') as File:  
-    reader = csv.reader(File)
-    for row in reader:
-        if name in row:
-            print(row)
+if os.access((filename), os.F_OK): 
+    print ('Given file path is exist.')
+
+if os.access((filename), os.R_OK):
+    print ('File is accessible to read')
+ 
+if os.access((filename), os.W_OK): 
+    print ('File is accessible to write')
+ 
+else:
+    print('no such file')
+
+
+txt = open(filename)
+
+print ('Содержимое файла %r' % filename)
+#print (txt.read())
+
+for string in txt:
+
+    if name in string:
+                   
+        print(string)
+
+
+
+    
+
